1eae3a8d1425e39160ab7068fee39735a6605a9d
SRIOS-TEACH <shannon.a.rios@gmail.com>
2026-04-10 01:37:26 +1000

    Update builder.js

--- JS/builder.js
+++ JS/builder.js
@@ -963,7 +963,9 @@ function extractChampionStatsFromBinRoot(raw, championName, pathName) {
   
   for (const [hashKey, statName] of Object.entries(CDRAGON_STAT_HASH_TO_NAME)) {
     const value = root[hashKey];
+    console.log(value);
     if (typeof value === "number" && Number.isFinite(value)) {
+      console.log("here");
       cdragonStats[statName] = value;
     }
   }
